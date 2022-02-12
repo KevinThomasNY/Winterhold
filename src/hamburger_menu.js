288445ed87ced8e1e8b57d669e1c56146ceb3dc8
Kevin Thomas <50027160+KevinThomasNY@users.noreply.github.com>
2022-02-12 14:27:22 -0500

    Add files via upload

---
+++ src/hamburger_menu.js
@@ -0,0 +1,21 @@
+/*
+Hamburger Menu
+The Hambuger Menu is used for opening and
+closing naviagtion menu in mobile/tablet view
+*/
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
